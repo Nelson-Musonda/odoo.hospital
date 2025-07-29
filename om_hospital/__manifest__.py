9bae32cff38143ad1f15f3af6c0151d9a92e48a6
Nelson Musonda <nelsonmusonda.mj@gmail.com>
2025-07-29 17:47:48 +0200

    My first commit

---
+++ om_hospital/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    "name": "Hospital",
+    "author": "Nelson Musonda",
+    "licence": "LGPL-3",
+    "version": "1.0.0",
+    "category": "health",
+    "data": [
+        "views/menu.xml"
+    ]
+}
